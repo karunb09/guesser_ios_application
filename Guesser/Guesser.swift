d3f9d65b1a1d10668c9e0ef121a05f709e2bffd8
Bourishetty,Karun <s533900@ol10037m.nwmissouri.edu>
2019-02-26 23:37:32 -0600

    fourth commit

--- Guesser/Guesser.swift
+++ Guesser/Guesser.swift
@@ -1,9 +1,58 @@
-//
-//  Guesser.swift
-//  Guesser
-//
-//  Created by Bourishetty,Karun on 2/26/19.
-//  Copyright © 2019 Bourishetty,Karun. All rights reserved.
-//
-
 import Foundation
+
+class Guesser {
+    private var correctAnswer:Int
+    private var _numattempts:Int
+    private var guesses: [Guess]
+    
+    struct Guess {
+        var correctAnswer:Int
+        var numAttempltsRequired:Int
+    }
+    
+    var numAttempts:Int{
+        return _numattempts
+    }
+    
+    init() {
+        correctAnswer = 0
+        _numattempts = 0
+        guesses = []
+    }
+    
+    private func createNewProblem() {
+        let random = Int.random(in: 0..<10) + 1
+        correctAnswer = random
+        _numattempts = 0
+    }
+    
+    func amIRight(guess: Int) -> String {
+        _numattempts = _numattempts + 1
+        //let generatedNum = createNewProblem()
+        if(guess > correctAnswer){
+            return Result.tooHigh.rawValue
+        }
+        else if(guess < correctAnswer){
+            //Too low
+            return Result.tooLow.rawValue
+        }
+        else{
+            guesses.append((Guesser.Guess).init(correctAnswer: correctAnswer, numAttempltsRequired: _numattempts))
+            createNewProblem()
+            return ""
+        }
+    }
+    
+    enum Result:String {
+        case tooLow = "Too Low"
+        case tooHigh = "Too High"
+        case correct = "Correct"
+    }
+    
+ 
+    
+    
+}
+
+
+
